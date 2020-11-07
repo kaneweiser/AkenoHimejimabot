47ecf0b33dab2dbf30b0046a6974486a1813ecdc
Rias <73129970+RiasGr3m0ry@users.noreply.github.com>
2020-11-07 06:15:03 -0800

    Update afk.py

--- AkenoHimejimabot/modules/afk.py
+++ AkenoHimejimabot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} is now away!{}".format(
+        update.effective_message.reply_text("{} Sayonara! 😘{}".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,9 +57,9 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} is here!', '{} is back!', '{} is now in the chat!',
-                '{} is awake!', '{} is back online!', '{} is finally here!',
-                'Welcome back! {}', 'Where is {}?\nIn the chat!'
+                '{} Ara Ara!', '{} Yōkoso!', '{} Rias missed you!',
+                '{} Why you came here?', '{} is wasting his time in this chat!', '{} Welcome back...Now pay 100$ or get banned.',
+                'Yamete...Yamete-kudasai {}-sama', 'Oh my! {} got no chills!!'
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
@@ -129,7 +129,7 @@ def check_afk(update, context, user_id, fst_name, userc_id):
         else:
             if int(userc_id) == int(user_id):
                 return
-            res = "{} is afk.\nReason: <code>{}</code>".format(
+            res = "{} is afk.\nReason: <code>{}</code>\nMaybe busy in babymaking! We should not disturb that person!".format(
                 html.escape(fst_name), html.escape(user.reason))
             update.effective_message.reply_text(res, parse_mode="html")
 

0dffbd5f4cc1e1d70488dad13fc66fe183ee5ba9
Rias <73129970+RiasGr3m0ry@users.noreply.github.com>
2020-11-12 11:57:21 -0800

    Update __main__.py

--- AkenoHimejimabot/__main__.py
+++ AkenoHimejimabot/__main__.py
@@ -556,7 +556,7 @@ def main():
 
     dispatcher.add_error_handler(error_callback)
 
-        if WEBHOOK:
+if WEBHOOK:
         LOGGER.info("Using webhooks.")
         updater.start_webhook(listen="0.0.0.0", port=PORT, url_path=TOKEN)
 

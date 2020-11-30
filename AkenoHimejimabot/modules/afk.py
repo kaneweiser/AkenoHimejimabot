4f6a5a6c44b67b814c08542232eb8f630106995c
Rias <73129970+RiasGr3m0ry@users.noreply.github.com>
2020-11-30 01:48:45 -0800

    Forgot what I did

--- AkenoHimejimabot/modules/afk.py
+++ AkenoHimejimabot/modules/afk.py
@@ -36,8 +36,8 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} Sayonara! 😘{}".format(
-            fname, notice))
+        update.effective_message.reply_text("{} *Sayonara {}!* #afk".format(
+            notice, fname))
     except BadRequest:
         pass
 
